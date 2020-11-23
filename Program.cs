19b09fd6ecb19c1aa213ca07ee7d767d0693eb91
Krieger251677 <69798247+Krieger251677@users.noreply.github.com>
2020-11-23 12:59:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Öğrencimisin?   (e/h)" );
+            string yanıt = Console.ReadLine();
+            if (yanıt=="e")
+            {
+                musteri ogrenci = new musteri();
+                ogrenci.ad = "Hasan";
+                ogrenci.soyad = "KAYA";
+                ogrenci.ogr_no = 221903054;
+                ogrenci.yas = 19;
+
+                Console.WriteLine(ogrenci.ad);
+
+            }
+            else if (yanıt == "h")
+            {
+                musteri müsteri = new musteri();
+                müsteri.ad = "Burak";
+                müsteri.soyad = "YILDIRIM";
+                müsteri.yas = 36;
+                müsteri.meslek = "Yazılım Mühendisi";
+                müsteri.maas = 5260;
+            }
+        }
+    }
+}
